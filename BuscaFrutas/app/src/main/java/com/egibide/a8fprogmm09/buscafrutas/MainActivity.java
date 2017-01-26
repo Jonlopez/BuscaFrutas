8b0a7b8b2931b5c985630306317984a35d417ec7
Unknown <jonlopezgarrido@gmail.com>
2017-01-26 21:51:46 +0100

    Estructura e inicialización de ActionBar en MainActivity

--- BuscaFrutas/app/src/main/java/com/egibide/a8fprogmm09/buscafrutas/MainActivity.java
+++ BuscaFrutas/app/src/main/java/com/egibide/a8fprogmm09/buscafrutas/MainActivity.java
@@ -2,6 +2,8 @@ package com.egibide.a8fprogmm09.buscafrutas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.bpersonaje:
+                //metodoAdd()
+                System.out.println("Se presionó cambiar personaje");
+                return true;
+            case R.id.instrucciones:
+                //metodoSearch()
+                System.out.println("Se presionó instrucciones");
+                return true;
+            case R.id.nuevo_juego:
+                //metodoEdit()
+                System.out.println("Se presionó nuevo juego");
+                return true;
+            case R.id.config:
+                //metodoDelete()
+                System.out.println("Se presionó configuracion del juego");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
